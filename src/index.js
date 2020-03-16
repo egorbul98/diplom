02f0a4e81bfda39f89baac1e7666cb65841d42e8
Egor Bulahtin <egorbul98@mail.ru>
2020-03-16 16:08:47 +0300

    webpack config, navigation

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
-import Post from "./js/Post"
-import $ from "jquery"
+// import Post from "./js/Post"
+// import $ from "jquery"
 
-//JSON
-import posts from "./js/json/posts.json"
-
-
-const post = new Post("Пост 1");
-const str = post.toString();
+// //JSON
+// import posts from "./js/json/posts.json"
 
 
-$(document).ready(function () {
-  let $parent = $('.list');
-  $parent.empty();
-  let str = '';
-  for (const key in posts) {
-    str += `
-    <h3 class="title">${posts[key].title}</h3>
-      <div class="content">${posts[key].price}</div>
-      <div class="footer">футер</div>
-    `
-  }
+// const post = new Post("Пост 1");
+// const str = post.toString();
 
-  $parent.append(str);
-});
+
+// $(document).ready(function () {
+//   let $parent = $('.list');
+//   $parent.empty();
+//   let str = '';
+//   for (const key in posts) {
+//     str += `
+//     <h3 class="title">${posts[key].title}</h3>
+//       <div class="content">${posts[key].price}</div>
+//       <div class="footer">футер</div>
+//     `
+//   }
+
+//   $parent.append(str);
+// });
+
+
 
 
 import "./sass/style.scss"
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
-
 // var ExtractTextPlugin = require("extract-text-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const packagejson = require('./package.json');
 const path = require('path')
+
+const isDev = process.env.NODE_ENV === 'development'
+console.log(isDev);
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    main: ["@babel/polyfill", "./src/index.js"],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -31,7 +42,44 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                autoprefixer({
+                  overrideBrowserslist: packagejson.browserslist
+                })
+              ],
+            }
+          },
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './img',
+              useRelativePath: true
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+            },
+          },
+        ],
       },
     ]
   },
@@ -39,6 +87,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     }),
-    new MiniCssExtractPlugin()
-  ]
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      { from: 'src/img', to: 'img' },
+      { from: 'src/fonts', to: 'fonts' },
+    ]),
+  ],
+  devtool: isDev ? "source-map" : "",
 }
